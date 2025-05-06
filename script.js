565c38eec9e589a1f5326802dc8da0dd9c915c69
Guilherme Miller <guilhermemiller700@gmail.com>
2025-05-06 00:00:05 -0300

    Projeto do trabalho prático

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("botaoMensagem").addEventListener("click",function(){
+    document.getElementById("mensagem").innerText= "Você é capaz de conquistar tudo o que deseja!";
+});
